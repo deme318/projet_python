2f60cd92fb003b2a5f6b7e87a08344295724fb4b
Mouhamed <89711786+deme318@users.noreply.github.com>
2023-06-02 11:23:32 +0200

    Update main.py
    Deuxième version

--- main.py
+++ main.py
@@ -17,22 +17,25 @@ lcd.clear(BLACK)
 # Clé d'API OpenWeatherMap (remplacez par votre propre clé)
 api_key = "610aa637f7a68197d131682923e2da75"
 
-# Récupération et affichage des données météorologiques pour chaque ville
-villes = ["Paris", "Marseille", "Lyon", "Toulouse", "Nice"]
-for ville in villes:
-    url = "http://api.openweathermap.org/data/2.5/weather?q={ville},fr&appid={api_key}&units=metric"
-    response = requests.get(url.format(ville=ville, api_key=api_key))
-    data = json.loads(response.text)
+# Ville pour récupérer les données météorologiques
+ville = "Rennes"
 
-    # Extraction des informations pertinentes
-    temperature = data["main"]["temp"]
-    description = data["weather"][0]["description"]
+url = "http://api.openweathermap.org/data/2.5/weather?q={ville},fr&appid={api_key}&units=metric"
+response = requests.get(url.format(ville=ville, api_key=api_key))
+data = json.loads(response.text)
 
-    # Affichage des informations sur l'écran M5Stack
-    lcd.print(ville, lcd.CENTER, 10, color=WHITE)
-    lcd.print("Température: {}°C".format(temperature), lcd.CENTER, 40, color=WHITE)
-    lcd.print("Description: {}".format(description), lcd.CENTER, 60, color=WHITE)
-    lcd.print("", lcd.CENTER, 80, color=WHITE)  # Ligne vide pour l'espace entre les villes
+# Extraction des informations pertinentes
+temperature = data["main"]["temp"]
+humidity = data["main"]["humidity"]
+wind_speed = data["wind"]["speed"]
+description = data["weather"][0]["description"]
+
+# Affichage des informations sur l'écran M5Stack
+lcd.print(ville, lcd.CENTER, 10, color=WHITE)
+lcd.print("Température: {}°C".format(temperature), lcd.CENTER, 40, color=WHITE)
+lcd.print("Humidité: {}%".format(humidity), lcd.CENTER, 60, color=WHITE)
+lcd.print("Vitesse du vent: {} m/s".format(wind_speed), lcd.CENTER, 80, color=WHITE)
+lcd.print("Description: {}".format(description), lcd.CENTER, 100, color=WHITE)
 
 # Attente avant de fermer l'écran
 input("Appuyez sur une touche pour fermer l'écran...")
